a9bee097cd8b0fd8878b3f11a29c573527e071fa
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2020-09-02 12:56:30 +0000

    Added mobile-specific event handling
    Fixes #15

--- src/app/room/canvas/canvas.component.ts
+++ src/app/room/canvas/canvas.component.ts
@@ -37,6 +37,10 @@ export class CanvasComponent implements AfterViewInit {
     this.captureEvents(this.canvasEl);
   }
 
+  preventDefault(event: MouseEvent) {
+      event.preventDefault();
+  }
+
   private captureEvents(canvasEl: HTMLCanvasElement) {
     // this will capture all mousedown events from the canvas element
     fromEvent(canvasEl, 'mousedown')
@@ -56,21 +60,48 @@ export class CanvasComponent implements AfterViewInit {
             );
         })
       )
-      .subscribe((res: [MouseEvent, MouseEvent]) => {
-        const rect = canvasEl.getBoundingClientRect();
+      .subscribe((res: [MouseEvent | TouchEvent, MouseEvent | TouchEvent]) => {
+        this.proccessMove(res)
+      });
+
+      fromEvent(canvasEl, 'touchstart')
+      .pipe(
+        switchMap((e) => {
+          // after a mouse down, we'll record all mouse moves
+          return fromEvent(canvasEl, 'touchmove')
+            .pipe(
+              // we'll stop (and unsubscribe) once the user releases the mouse
+              // this will trigger a 'mouseup' event
+              takeUntil(fromEvent(canvasEl, 'touchend')),
+              // we'll also stop (and unsubscribe) once the mouse leaves the canvas (mouseleave event)
+              takeUntil(fromEvent(canvasEl, 'touchleave')),
+              // pairwise lets us get the previous value to draw a line from
+              // the previous point to the current point
+              pairwise()
+            );
+        })
+      )
+      .subscribe((res: [MouseEvent | TouchEvent, MouseEvent | TouchEvent]) => {
+        this.proccessMove(res)
+      });
+  }
+
+    private proccessMove(res: [MouseEvent | TouchEvent, MouseEvent | TouchEvent]) {
+        const rect = this.canvasEl.getBoundingClientRect();
         // previous and current position with the offset
+        const points: [{clientX: number, clientY: number}, {clientX: number, clientY: number}] =
+        res[0] instanceof MouseEvent ? (res as [MouseEvent, MouseEvent]) : [res[0].touches[0], (res[1] as TouchEvent).touches[0]];
         const prevPos = {
-          x: res[0].clientX - rect.left,
-          y: res[0].clientY - rect.top
+            x: points[0].clientX - rect.left,
+            y: points[0].clientY - rect.top
         };
         const currentPos = {
-          x: res[1].clientX - rect.left,
-          y: res[1].clientY - rect.top
+            x: points[1].clientX - rect.left,
+            y: points[1].clientY - rect.top
         };
         // this method we'll implement soon to do the actual drawing
         this.drawOnCanvas(prevPos, currentPos);
-      });
-  }
+    }
 
   private drawOnCanvas(prevPos: { x: number, y: number }, currentPos: { x: number, y: number }) {
     if (!this.cx) { return; }
